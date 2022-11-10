db92ec147032f40979e2213eb53418c008f8b6b4
Rafiq1337 <71429163+Rafix1337@users.noreply.github.com>
2022-11-10 18:57:23 +0100

    Testing sharpening function

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -40,7 +40,8 @@ namespace WindowsFormsApp1
             try
             {
                 OpenFileDialog dialog = new OpenFileDialog();
-                dialog.Filter = "jpg files(.*jpg)|*.jpg| BMP files(.*bmp)|*.bmp| All Files(*.*)|*.*";
+/*                dialog.Filter = "jpg files(.*jpg)|*.jpg| BMP files(.*bmp)|*.bmp| All Files(*.*)|*.*";*/
+                dialog.Filter = "All Files(*.*)|*.*| jpg files(.*jpg)|*.jpg| BMP files(.*bmp)|*.bmp";
 
                 if (dialog.ShowDialog() == DialogResult.OK)
                 {
@@ -87,7 +88,9 @@ namespace WindowsFormsApp1
         private void button2_Click(object sender, EventArgs e)
         {
             Bitmap copyBitmap = new Bitmap((Bitmap)pictureBox1.Image);
-            ProcessImage(copyBitmap);
+/*            ProcessImage(copyBitmap);*/
+              sharp(copyBitmap);
+
             pictureBox2.Image = copyBitmap;
 
         }
@@ -115,5 +118,148 @@ namespace WindowsFormsApp1
             }
             return true;
         }
+
+        /*       public static Bitmap Sharpen(Bitmap image)
+               {
+                   Bitmap sharpenImage = (Bitmap)image.Clone();
+
+                   const int filterWidth = 5;
+                   const int filterHeight = 5;
+                   int width = image.Width;
+                   int height = image.Height;
+
+                   // Create sharpening filter.
+                   double[,] filter = new double[filterWidth, filterHeight] {
+           { -1, -1, -1, -1, -1 },
+           { -1,  2,  2,  2, -1 },
+           { -1,  2,  100,  2, -1 },
+           { -1,  2,  2,  2, -1 },
+           { -1, -1, -1, -1, -1 }
+       };
+
+                   double factor = 1.0 / 100.0;
+                   double bias = 0.0;
+
+                   Color[,] result = new Color[image.Width, image.Height];
+
+                   // Lock image bits for read/write.
+                   BitmapData pbits = sharpenImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+                   // Declare an array to hold the bytes of the bitmap.
+                   int bytes = pbits.Stride * height;
+                   byte[] rgbValues = new byte[bytes];
+
+                   // Copy the RGB values into the array.
+                   System.Runtime.InteropServices.Marshal.Copy(pbits.Scan0, rgbValues, 0, bytes);
+
+                   int rgb;
+                   // Fill the color array with the new sharpened color values.
+                   for (int x = 0; x < width; ++x)
+                   {
+                       for (int y = 0; y < height; ++y)
+                       {
+                           double red = 0.0, green = 0.0, blue = 0.0;
+
+                           for (int filterX = 0; filterX < filterWidth; filterX++)
+                           {
+                               for (int filterY = 0; filterY < filterHeight; filterY++)
+                               {
+                                   int imageX = (x - filterWidth / 2 + filterX + width) % width;
+                                   int imageY = (y - filterHeight / 2 + filterY + height) % height;
+
+                                   rgb = imageY * pbits.Stride + 3 * imageX;
+
+                                   red += rgbValues[rgb + 2] * filter[filterX, filterY];
+                                   green += rgbValues[rgb + 1] * filter[filterX, filterY];
+                                   blue += rgbValues[rgb + 0] * filter[filterX, filterY];
+                               }
+                               int r = Math.Min(Math.Max((int)(factor * red + bias), 0), 255);
+                               int g = Math.Min(Math.Max((int)(factor * green + bias), 0), 255);
+                               int b = Math.Min(Math.Max((int)(factor * blue + bias), 0), 255);
+
+                               result[x, y] = Color.FromArgb(r, g, b);
+                           }
+                       }
+                   }
+
+                   // Update the image with the sharpened pixels.
+                   for (int x = 0; x < width; ++x)
+                   {
+                       for (int y = 0; y < height; ++y)
+                       {
+                           rgb = y * pbits.Stride + 3 * x;
+
+                           rgbValues[rgb + 2] = result[x, y].R;
+                           rgbValues[rgb + 1] = result[x, y].G;
+                           rgbValues[rgb + 0] = result[x, y].B;
+                       }
+                   }
+
+                   // Copy the RGB values back to the bitmap.
+                   System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, pbits.Scan0, bytes);
+                   // Release image bits.
+                   sharpenImage.UnlockBits(pbits);
+
+                   return sharpenImage;
+               }*/
+        public static Bitmap sharp(Bitmap img)
+        {
+            Bitmap sharpenImage = new Bitmap(img.Width, img.Height);
+
+            int filterWidth = 3;
+            int filterHeight = 3;
+            int w = img.Width;
+            int h = img.Height;
+
+            double[,] filter = new double[filterWidth, filterHeight];
+
+            filter[0, 0] = filter[0, 1] = filter[0, 2] = filter[1, 0] = filter[1, 2] =
+           filter[2, 0] = filter[2, 1] = filter[2, 2] = -1;
+            filter[1, 1] = 9;
+
+            double factor = 1.0;
+            double bias = 0.0;
+
+            Color[,] result = new Color[img.Width, img.Height];
+
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    double red = 0.0, green = 0.0, blue = 0.0;
+
+
+                    for (int filterX = 0; filterX < filterWidth; filterX++)
+                    {
+                        for (int filterY = 0; filterY < filterHeight; filterY++)
+                        {
+                            int imageX = (x - filterWidth / 2 + filterX + w) % w;
+                            int imageY = (y - filterHeight / 2 + filterY + h) % h;
+
+                            Color imageColor = img.GetPixel(imageX, imageY);
+                            red += imageColor.R * filter[filterX, filterY];
+                            green += imageColor.G * filter[filterX, filterY];
+                            blue += imageColor.B * filter[filterX, filterY];
+                        }
+                        int r = Math.Min(Math.Max((int)(factor * red + bias), 0), 255);
+                        int g = Math.Min(Math.Max((int)(factor * green + bias), 0), 255);
+                        int b = Math.Min(Math.Max((int)(factor * blue + bias), 0), 255);
+
+                        result[x, y] = Color.FromArgb(r, g, b);
+                    }
+                }
+            }
+            for (int i = 0; i < w; ++i)
+            {
+                for (int j = 0; j < h; ++j)
+                {
+                    sharpenImage.SetPixel(i, j, result[i, j]);
+                }
+            }
+            return sharpenImage;
+        }
+
+
+
     }
 }
