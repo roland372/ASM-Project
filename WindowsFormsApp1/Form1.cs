51456392abe31429d95f9fb51bd81274ac2c6cd7
roland372 <damfat94@gmail.com>
2022-11-21 23:51:26 +0100

    refactored code

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -18,7 +18,6 @@ using System.Globalization;
 using System.Collections.Specialized;
 
 namespace WindowsFormsApp1
-   
 {
     public partial class Form1 : Form
     {
@@ -28,15 +27,11 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
-
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             String imageLocation = "";
-
 
             try
             {
@@ -64,10 +59,7 @@ namespace WindowsFormsApp1
                     Console.WriteLine("Image Pixel depth : " + Image.GetPixelFormatSize(img.PixelFormat));
 
                     pictureBox1.Visible = true;
-
                     pictureBox1.Update();
-
-
 
                 }
 
@@ -76,17 +68,15 @@ namespace WindowsFormsApp1
             {
                 MessageBox.Show("Error", "Error occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if(pictureBox1.Image != null)
+            if (pictureBox1.Image != null)
             {
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 Bitmap copyBitmap = new Bitmap((Bitmap)pictureBox1.Image);
                 double d1 = double.Parse(value, CultureInfo.InvariantCulture);
-
 
                 pictureBox2.Image = SubtractAddFactorImage(copyBitmap, ConvolutionFilter(copyBitmap, GaussianBlur(5, d1)));
 
@@ -102,17 +92,16 @@ namespace WindowsFormsApp1
                 MessageBox.Show("Please select image.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
-
         }
 
         public bool ProcessImage(Bitmap bmp)
         {
-            for (int i =0; i < bmp.Width; ++i)
-                {
-                for (int j =0; j < bmp.Height; ++j)
+            for (int i = 0; i < bmp.Width; ++i)
+            {
+                for (int j = 0; j < bmp.Height; ++j)
                 {
                     Color bmpColor = bmp.GetPixel(i, j);
-                    int red = bmpColor.R; 
+                    int red = bmpColor.R;
                     int green = bmpColor.G;
                     int blue = bmpColor.B;
                     int gray = (byte)(.299 * red + .587 * green + .114 * blue);
@@ -122,13 +111,10 @@ namespace WindowsFormsApp1
 
                     bmp.SetPixel(i, j, Color.FromArgb(red, green, blue));
 
-                  
                 }
-                
             }
             return true;
         }
-
 
         public static Bitmap sharp(Bitmap img)
         {
@@ -142,7 +128,7 @@ namespace WindowsFormsApp1
             double[,] filter = new double[filterWidth, filterHeight];
 
             filter[0, 0] = filter[0, 1] = filter[0, 2] = filter[1, 0] = filter[1, 2] =
-           filter[2, 0] = filter[2, 1] = filter[2, 2] = -1;
+            filter[2, 0] = filter[2, 1] = filter[2, 2] = -1;
             filter[1, 1] = 9;
 
             double factor = 1.0;
@@ -155,7 +141,6 @@ namespace WindowsFormsApp1
                 for (int y = 0; y < h; ++y)
                 {
                     double red = 0.0, green = 0.0, blue = 0.0;
-
 
                     for (int filterX = 0; filterX < filterWidth; filterX++)
                     {
@@ -187,37 +172,29 @@ namespace WindowsFormsApp1
             return sharpenImage;
         }
 
-        public static Bitmap ConvolutionFilter(Bitmap sourceBitmap,
-                                    double[,] filterMatrix,
-                                         double factor = 1,
-                                              int bias = 0,
-                                    bool grayscale = false)
+        public static Bitmap ConvolutionFilter(Bitmap sourceBitmap, double[,] filterMatrix, double factor = 1, int bias = 0, bool grayscale = false)
         {
             BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
-                                     sourceBitmap.Width, sourceBitmap.Height),
-                                                       ImageLockMode.ReadOnly,
-                                                 PixelFormat.Format32bppArgb);
-
+                                    sourceBitmap.Width,
+                                    sourceBitmap.Height),
+                                    ImageLockMode.ReadOnly,
+                                    PixelFormat.Format32bppArgb);
 
             byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
             byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
 
-
             Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
             sourceBitmap.UnlockBits(sourceData);
-
 
             if (grayscale == true)
             {
                 float rgb = 0;
-
 
                 for (int k = 0; k < pixelBuffer.Length; k += 4)
                 {
                     rgb = pixelBuffer[k] * 0.11f;
                     rgb += pixelBuffer[k + 1] * 0.59f;
                     rgb += pixelBuffer[k + 2] * 0.3f;
-
 
                     pixelBuffer[k] = (byte)rgb;
                     pixelBuffer[k + 1] = pixelBuffer[k];
@@ -226,91 +203,60 @@ namespace WindowsFormsApp1
                 }
             }
 
-
             double blue = 0.0;
             double green = 0.0;
             double red = 0.0;
 
-
             int filterWidth = filterMatrix.GetLength(1);
             int filterHeight = filterMatrix.GetLength(0);
-
 
             int filterOffset = (filterWidth - 1) / 2;
             int calcOffset = 0;
 
-
             int byteOffset = 0;
 
-
-            for (int offsetY = filterOffset; offsetY <
-                sourceBitmap.Height - filterOffset; offsetY++)
+            for (int offsetY = filterOffset; offsetY < sourceBitmap.Height - filterOffset; offsetY++)
             {
-                for (int offsetX = filterOffset; offsetX <
-                    sourceBitmap.Width - filterOffset; offsetX++)
+                for (int offsetX = filterOffset; offsetX < sourceBitmap.Width - filterOffset; offsetX++)
                 {
                     blue = 0;
                     green = 0;
                     red = 0;
 
+                    byteOffset = offsetY * sourceData.Stride + offsetX * 4;
 
-                    byteOffset = offsetY *
-                                 sourceData.Stride +
-                                 offsetX * 4;
-
-
-                    for (int filterY = -filterOffset;
-                        filterY <= filterOffset; filterY++)
+                    for (int filterY = -filterOffset; filterY <= filterOffset; filterY++)
                     {
-                        for (int filterX = -filterOffset;
-                            filterX <= filterOffset; filterX++)
+                        for (int filterX = -filterOffset; filterX <= filterOffset; filterX++)
                         {
+                            calcOffset = byteOffset + (filterX * 4) + (filterY * sourceData.Stride);
 
+                            blue += (double)(pixelBuffer[calcOffset]) * filterMatrix[filterY + filterOffset, filterX + filterOffset];
 
-                            calcOffset = byteOffset +
-                                         (filterX * 4) +
-                                         (filterY * sourceData.Stride);
+                            green += (double)(pixelBuffer[calcOffset + 1]) * filterMatrix[filterY + filterOffset, filterX + filterOffset];
 
-
-                            blue += (double)(pixelBuffer[calcOffset]) *
-                                    filterMatrix[filterY + filterOffset,
-                                                 filterX + filterOffset];
-
-
-                            green += (double)(pixelBuffer[calcOffset + 1]) *
-                                     filterMatrix[filterY + filterOffset,
-                                                  filterX + filterOffset];
-
-
-                            red += (double)(pixelBuffer[calcOffset + 2]) *
-                                   filterMatrix[filterY + filterOffset,
-                                                filterX + filterOffset];
+                            red += (double)(pixelBuffer[calcOffset + 2]) * filterMatrix[filterY + filterOffset, filterX + filterOffset];
                         }
                     }
-
 
                     blue = factor * blue + bias;
                     green = factor * green + bias;
                     red = factor * red + bias;
-
 
                     if (blue > 255)
                     { blue = 255; }
                     else if (blue < 0)
                     { blue = 0; }
 
-
                     if (green > 255)
                     { green = 255; }
                     else if (green < 0)
                     { green = 0; }
 
-
                     if (red > 255)
                     { red = 255; }
                     else if (red < 0)
                     { red = 0; }
-
 
                     resultBuffer[byteOffset] = (byte)(blue);
                     resultBuffer[byteOffset + 1] = (byte)(green);
@@ -319,25 +265,18 @@ namespace WindowsFormsApp1
                 }
             }
 
-
-            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width,
-                                             sourceBitmap.Height);
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
 
             BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
                                      resultBitmap.Width, resultBitmap.Height),
-                                                      ImageLockMode.WriteOnly,
-                                                 PixelFormat.Format32bppArgb);
-
+                                     ImageLockMode.WriteOnly,
+                                     PixelFormat.Format32bppArgb);
 
             Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
             resultBitmap.UnlockBits(resultData);
 
-
             return resultBitmap;
         }
-
-
-
 
         private static Bitmap SubtractAddFactorImage(
                               Bitmap subtractFrom,
@@ -350,18 +289,13 @@ namespace WindowsFormsApp1
                        ImageLockMode.ReadOnly,
                        PixelFormat.Format32bppArgb);
 
-
-            byte[] sourceBuffer = new byte[sourceData.Stride *
-                                           sourceData.Height];
+            byte[] sourceBuffer = new byte[sourceData.Stride * sourceData.Height];
 
 
-            Marshal.Copy(sourceData.Scan0, sourceBuffer, 0,
-                                        sourceBuffer.Length);
+            Marshal.Copy(sourceData.Scan0, sourceBuffer, 0, sourceBuffer.Length);
 
 
-            byte[] resultBuffer = new byte[sourceData.Stride *
-                                           sourceData.Height];
-
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
 
             BitmapData subtractData =
                        subtractValue.LockBits(new Rectangle(0, 0,
@@ -369,46 +303,28 @@ namespace WindowsFormsApp1
                        ImageLockMode.ReadOnly,
                        PixelFormat.Format32bppArgb);
 
+            byte[] subtractBuffer = new byte[subtractData.Stride * subtractData.Height];
 
-            byte[] subtractBuffer = new byte[subtractData.Stride *
-                                             subtractData.Height];
-
-
-            Marshal.Copy(subtractData.Scan0, subtractBuffer, 0,
-                                         subtractBuffer.Length);
-
+            Marshal.Copy(subtractData.Scan0, subtractBuffer, 0, subtractBuffer.Length);
 
             subtractFrom.UnlockBits(sourceData);
             subtractValue.UnlockBits(subtractData);
-
 
             double blue = 0;
             double green = 0;
             double red = 0;
 
-
-            for (int k = 0; k < resultBuffer.Length &&
-                           k < subtractBuffer.Length; k += 4)
+            for (int k = 0; k < resultBuffer.Length && k < subtractBuffer.Length; k += 4)
             {
-                blue = sourceBuffer[k] +
-                      (sourceBuffer[k] -
-                       subtractBuffer[k]) * factor;
+                blue = sourceBuffer[k] + (sourceBuffer[k] - subtractBuffer[k]) * factor;
 
+                green = sourceBuffer[k + 1] + (sourceBuffer[k + 1] - subtractBuffer[k + 1]) * factor;
 
-                green = sourceBuffer[k + 1] +
-                       (sourceBuffer[k + 1] -
-                        subtractBuffer[k + 1]) * factor;
-
-
-                red = sourceBuffer[k + 2] +
-                     (sourceBuffer[k + 2] -
-                      subtractBuffer[k + 2]) * factor;
-
+                red = sourceBuffer[k + 2] + (sourceBuffer[k + 2] - subtractBuffer[k + 2]) * factor;
 
                 blue = (blue < 0 ? 0 : (blue > 255 ? 255 : blue));
                 green = (green < 0 ? 0 : (green > 255 ? 255 : green));
                 red = (red < 0 ? 0 : (red > 255 ? 255 : red));
-
 
                 resultBuffer[k] = (byte)blue;
                 resultBuffer[k + 1] = (byte)green;
@@ -416,10 +332,7 @@ namespace WindowsFormsApp1
                 resultBuffer[k + 3] = 255;
             }
 
-
-            Bitmap resultBitmap = new Bitmap(subtractFrom.Width,
-                                             subtractFrom.Height);
-
+            Bitmap resultBitmap = new Bitmap(subtractFrom.Width, subtractFrom.Height);
 
             BitmapData resultData =
                        resultBitmap.LockBits(new Rectangle(0, 0,
@@ -427,26 +340,15 @@ namespace WindowsFormsApp1
                        ImageLockMode.WriteOnly,
                        PixelFormat.Format32bppArgb);
 
-
-            Marshal.Copy(resultBuffer, 0, resultData.Scan0,
-                                       resultBuffer.Length);
-
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
 
             resultBitmap.UnlockBits(resultData);
-
 
             return resultBitmap;
         }
 
-
-
-
-
-
-
         public static double[,] GaussianBlur(int lenght, double weight)
         {
-/*            Console.WriteLine("trackbar value : " + value);*/
             double[,] kernel = new double[lenght, lenght];
             double kernelSum = 0;
             int foff = (lenght - 1) / 2;
@@ -468,8 +370,6 @@ namespace WindowsFormsApp1
                     kernel[y, x] = kernel[y, x] * 1d / kernelSum;
                 }
             }
-/*            Console.WriteLine("test : " + kernel);*/
-
             return kernel;
         }
 
@@ -477,18 +377,23 @@ namespace WindowsFormsApp1
         {
             int width = srcImage.Width;
             int height = srcImage.Height;
-            BitmapData srcData = srcImage.LockBits(new Rectangle(0, 0, width, height),
-                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            BitmapData srcData = srcImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
             int bytes = srcData.Stride * srcData.Height;
             byte[] buffer = new byte[bytes];
             byte[] result = new byte[bytes];
+
             Marshal.Copy(srcData.Scan0, buffer, 0, bytes);
+
             srcImage.UnlockBits(srcData);
+
             int colorChannels = 3;
             double[] rgb = new double[colorChannels];
             int foff = (kernel.GetLength(0) - 1) / 2;
             int kcenter = 0;
             int kpixel = 0;
+
             for (int y = foff; y < height - foff; y++)
             {
                 for (int x = foff; x < width - foff; x++)
@@ -528,23 +433,16 @@ namespace WindowsFormsApp1
                 }
             }
             Bitmap resultImage = new Bitmap(width, height);
-            BitmapData resultData = resultImage.LockBits(new Rectangle(0, 0, width, height),
-                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            BitmapData resultData = resultImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
             Marshal.Copy(result, 0, resultData.Scan0, bytes);
+
             resultImage.UnlockBits(resultData);
             return resultImage;
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            /*Console.WriteLine("trackbar value : " + trackBar1.Value.ToString());*/
             value = trackBar1.Value.ToString();
-
-/*            Bitmap copyBitmap = new Bitmap((Bitmap)pictureBox1.Image);
-
-            double d1 = double.Parse(value, CultureInfo.InvariantCulture);
-
-            pictureBox2.Image = SubtractAddFactorImage(copyBitmap, ConvolutionFilter(copyBitmap, GaussianBlur(5, d1)));*/
 
         }
 
